b6a64c02cd9651450e7d6c868f2d13f39f9f947f
Adriano Dias <adrianodias7000@gmail.com>
2020-12-22 17:54:54 -0300

    Faltou commit do arquivo main.js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let nome = "adriano soares";
+let idade = "35";
+let frase = "Japão é a melhor seleção do Mundo!";
+
+
+//alert("Meu primeiro JS =D");
+//alert("Bem vindo "+ nome+ " tem "+ idade+" anos");
+//alert("multiplicando idade por 3 = " + idade * 3);
+//alert(idade*3)
+console.log(nome);
+console.log(frase.toUpperCase());
+console.log(frase.replace("Japão","Brasil"));
+console.log(frase.toLowerCase());
+frase = frase.replace("melhor","guerreira das");
+console.log(frase)
+//console.log(idade + idade2)
