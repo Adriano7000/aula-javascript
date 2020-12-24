ef546fd20e287ee503b5b8312329e1cccc273db0
Adriano Dias <adrianodias7000@gmail.com>
2020-12-23 21:55:27 -0300

    Primeira Parte de Intoduçao ao JavaScript realizada

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
+/* 
 let nome = "adriano soares";
 let idade = "35";
 let frase = "Japão é a melhor seleção do Mundo!";
-
+let lista = ["maça","laranja","manga","uva"];
 
 //alert("Meu primeiro JS =D");
 //alert("Bem vindo "+ nome+ " tem "+ idade+" anos");
@@ -14,3 +15,31 @@ console.log(frase.toLowerCase());
 frase = frase.replace("melhor","guerreira das");
 console.log(frase)
 //console.log(idade + idade2)
+
+console.log(lista);
+lista.pop();
+lista.push("pera");
+console.log(lista.length);
+console.log(lista);
+console.log(lista[2]);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(' - '));
+console.log(lista.join(' | '));
+*/ 
+var fruta = [{nome : "maca",cor : "vermelha"},{nome : "uva",cor : "roxa"}];
+console.log(fruta);
+console.log(fruta[1].nome);
+//console.log(fruta);
+
+
+
+
+/*
+var texto = "javascript devmedia";
+  alert(texto["length"]);
+  alert(texto.length);
+
+var vazio = null;
+alert(vazio.length);
+*/
---
+++ js/main_2.js
@@ -0,0 +1,35 @@
+
+
+/*
+var  idade = prompt("Qual a sua idade? ");
+if (idade >=  18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}
+*/
+/*
+var count = 0;
+while (count < 5){
+    console.log(count);
+    //alert(count)
+    count++ ;
+}
+
+for(count=0; count <= 5; count++){
+    console.log(count);
+}
+*/
+
+var d = new Date();
+//alert(d);
+console.log(d.getMonth()+1);
+console.log(d.getTime());
+console.log(d.getDate());
+console.log(d.getUTCDate());
+console.log(d.getMinutes());
+console.log(d.getHours());
+
+
+
+
